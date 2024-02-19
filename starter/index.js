e6554000c920e013c9bee5a4d9c3a4865855762d
RajpreetKR <rajpreet@khalsa.com>
2024-02-19 20:56:29 +0000

    added to init and writetofile functions with tutor

--- starter/index.js
+++ starter/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs");   // Here we are BRINGING in REFERENCE to the File SYSTEM Library
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
@@ -38,17 +38,51 @@ const questions = [
     {
         type: "input",
         name: "questions",
-        message: "Not sure what to put here:",
+        message: "Please enter commonly asked questions regarding your project:",
     }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    // Q: What are the INPUTs and what (if any) are the OUTPUTS (?)
+    // INPUTS --> filename: 'README.md'
+    //        --> data: What we collected from the USER (inquirer)
+
+    // using the FS library we want to WRITE the collected DATA (from inquirer) to a FILE (README.md)
+
+    // OUTPUTS --> we would want to call our gerneratemarkdown() function 
+    //           --> pass the USER collected data --> generateMarkdown(answers)
+
+    var readmeData = generateMarkdown(data);
+    console.log("Genereated Content: ", readmeData);
+
+   // OUTPUT --> WRITING TEH MARKDOWN TO the File System
+    fs.writeFile('README.md', readmeData, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 }
 
 // function to initialize program
 function init() {
-
+    // lets explore hte INQUIERE LIB
+    console.log("I am code BEFORE the ASYNC propmt method");
+    inquirer.prompt(    // the prompt async method returns a PROMISE
+        questions
+    )
+    .then(function(answers) {
+       // console.log("Data: ", answers);
+       // console.log("Repo Title: ", answers.title);
+        // Where does this newxt function call belong?
+        console.log("I am code INSIDE the ASYNC propmt method");
+        writeToFile('READEME.md', answers)
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+    
+    // Where does this newxt function call belong?
+    console.log("I am code AFTER the ASYNC propmt method");
 }
 
 // function call to initialize program
