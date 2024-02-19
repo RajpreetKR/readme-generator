2fe51bd9ddc9f434c9195431bd24f10f30982ebd
RajpreetKR <rajpreet@khalsa.com>
2024-02-19 13:30:58 +0000

    added question for user to answer

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,41 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your project title:",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter your project description:",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What does the user need to do to install your project?:",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please enter your desired license:",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please enter any collaborators you worked with:",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What does the user need in order to test your project?",
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Not sure what to put here:",
+    }
 ];
 
 // function to write README file
